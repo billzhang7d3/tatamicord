2ce1a5edfdbea1437b3a93f5691fc240ae0427a8
Bill Zhang <xzhan423@ucsc.edu>
2025-06-30 21:49:16 -0700

    make cors accept requests and headers

--- backend/src/app.rs
+++ backend/src/app.rs
@@ -1,12 +1,15 @@
-use std::{
-    sync::Arc,
-    env
-};
 use axum::{
     routing::{post},
     Router
 };
+use http::header::{AUTHORIZATION, CONTENT_TYPE};
+use std::{
+    sync::Arc,
+    env
+};
 use tokio_postgres::{Client, Config, NoTls};
+use tower::ServiceBuilder;
+use tower_http::cors::{Any, CorsLayer};
 
 use crate::handlers;
 
@@ -34,11 +37,17 @@ async fn create_client() -> Client {
 }
 
 pub async fn create_app() -> Router {
+    let cors = CorsLayer::new()
+        .allow_headers([AUTHORIZATION, CONTENT_TYPE])
+        .allow_origin(Any);
     let client_arc = Arc::new(create_client().await);
     let client_clone = Arc::clone(&client_arc);
     return Router::new()
         .route("/register/", post(handlers::register_handler))
         .route("/login/", post(handlers::login_handler))
-        .with_state(client_clone);
+        .with_state(client_clone)
+        .layer(ServiceBuilder::new()
+            .layer(cors));
         // .layer(TraceLayer::new_for_http())
+    // TODO: make middleware actually good
 }
